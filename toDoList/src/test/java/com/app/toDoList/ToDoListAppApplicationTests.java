1350eead5936ddde17744f85ad1505ad46d8ad50
Jbfaneto <jbfaneto@gmail.com>
2023-10-04 12:30:20 -0300

    First coommit to the project

---
+++ toDoList/src/test/java/com/app/toDoList/ToDoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
